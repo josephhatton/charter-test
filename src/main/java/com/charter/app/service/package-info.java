24bb17ffd6edd257c3da27eb2e65d109c704f9c2
josephhatton <joseph_hatton@yahoo.com>
2020-02-02 22:50:07 -0600

    This is for Charter Test

---
+++ src/main/java/com/charter/app/service/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Service layer beans.
+ */
+package com.charter.app.service;
---
+++ src/main/java/com/charter/app/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.charter.app.web.rest.vm;
